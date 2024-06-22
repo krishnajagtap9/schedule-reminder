66d41e8474ffcf69c3c7bb7851bfb0a595a5991a
krishnajagtap9 <jagtapkanaha@gmail.com>
2024-06-22 19:18:13 +0530

    Add files via upload

---
+++ public/developer.js
@@ -0,0 +1,49 @@
+let man=()=>{
+    let typing_animation1 =document.getElementById("A1")
+    let typing_animation2 =document.getElementById("A2")
+    let typing_animation3 =document.getElementById("A3")
+   let text_images= document.getElementById("text_images")
+   let text_images2= document.getElementById("text_images2")
+    typing_animation2.style.display="none"
+
+    setTimeout(()=>{
+        typing_animation2.style.display="block"
+
+    },3000)
+  
+    
+    setTimeout(()=>{
+typing_animation3.style.display = "block"
+
+    },6500)
+
+    setTimeout(()=>{
+
+let logo = document.querySelector(".logo")
+logo.className="logo_after"
+    },9500)
+
+
+//     setTimeout(()=>{
+// typing_animation1.innerHTML= "Then this website help you in remembering your important "
+// typing_animation1.style.display="block"
+//     },11000)
+
+//     setTimeout(()=>{
+// typing_animation2.innerHTML= "Schedule of the current date as will as expired date!!"
+//         typing_animation2.style.display="block"
+
+//     },16000)
+
+//     setTimeout(()=>{
+//         text_images.style.display="block"
+//         text_images.style.backgroundImage="url( ./images/question-answer-scene-2-people-260nw-1394371715.webp)"
+//             },18000)
+//      setTimeout(()=>{
+//                 text_images2.style.display="block"
+//                     },19500)
+
+    
+
+}
+man()
